385d9535405b890a3f9cbf66616dbd818368921c
cs16mmp <1522680@brunel.ac.uk>
2020-02-13 19:25:29 +0000

    First commit

--- components/ListComponent.js
+++ components/ListComponent.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import { View } from "react-native";
 import { ListItem } from "react-native-elements";
+import SecureStore from 'expo-secure-store';
 
 class ListComponent extends Component {  
     
     _onPressItem(nextScreen, currentScreen, value) {
         this.props.navigation.navigate(nextScreen)
-        this._storeData(currentScreen, value)
-        console.warn("Value Stored", value);  
-    
+        this._storeData(currentScreen, value) 
       }
       async _storeData(currentScreen, value) {  
         try {
+          console.warn("Value Stored", value); 
           await SecureStore.setItemAsync(currentScreen, value)
         } catch (e) {
           console.log(e)
--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import {
-  SafeAreaView,
   View,
   FlatList,
   StyleSheet,
   Text,
   TouchableHighlight
 } from "react-native";
+import SecureStore from 'expo-secure-store';
 
-const DATA = [
+const orientation = "none";
+const gender = "none";
+
+const Profile1 = [
   {
     id: "1",
     title: "Closest Clinic"
@@ -34,6 +37,37 @@ const DATA = [
     title: "Mental Health"
   }
 ];
+const Profile2= [
+  {
+    id: "1",
+    title: "Closest Clinic"
+  },
+  {
+    id: "2",
+    title: "Clinics"
+  },
+  {
+    id: "3",
+    title: "Emergency contraception"
+  },
+  {
+    id: "4",
+    title: "Contraception"
+  },
+  {
+    id: "5",
+    title: "Abortion support"
+  },
+  {
+    id: "6",
+    title: "Sexual assault"
+  },
+  {
+    id: "7",
+    title: "Mental Health"
+  }
+];
+
 function Item({ title }) {
   return (
     <View style={styles.item}>
@@ -43,16 +77,40 @@ function Item({ title }) {
 }
 
 class homeScreen extends Component {
+  
+  _onPressItem() {
+    this._getData()
+  }
+  async _getData(){
+    try {
+      gender = await SecureStore.getItemAsync("genderScreen")
+      orientation = await SecureStore.getItemAsync("orientationScreen")
+      console.warn(gender)
+      console.warn(orientation)
+    } catch (e) {
+      console.log(e)
+    }
+    console.warn(gender)
+    console.warn(orientation)
+
+    // if (gender == "Male Cis" && orientation == "Heterosexual") {
+    //   return Profile1
+    // }
+    // return Profile2
+
+  }
   static navigationOptions = {
     headerShown: false
   };
   render() {
+    this._getData
     return (
       <FlatList
-        data={DATA}
+        data={Profile1}
         renderItem={({ item }) => (
           <TouchableHighlight
-            onPress={() => this.props.navigation.navigate("ItemScreen")}>
+            // onPress={() => this.props.navigation.navigate("ItemScreen")}>
+            onPress={() => this._onPressItem()}>
             <Item title={item.title} />
           </TouchableHighlight>
         )}
